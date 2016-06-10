05fea9af1f6c435c3661ceb4f99f3b516e954873
Michael Kahn <mike.kahn@irdeto.com>
2016-06-10 14:19:15 -0400

    Fixed the default Access-Control-Allow-Headers to allow headers required by Chrome, Firefox, AngularJS.  If only the "Authorization" header is allowed, as per the previous version, nearly non-GET CORS requests will fail.

--- src/main/java/com/auth0/spring/security/auth0/Auth0CORSFilter.java
+++ src/main/java/com/auth0/spring/security/auth0/Auth0CORSFilter.java
@@ -17,7 +17,7 @@ public class Auth0CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Content-Length, Accept, X-Requested-With, X-XSRF-TOKEN");
         if (request.getMethod().equals("OPTIONS")) {
             return;
         }
